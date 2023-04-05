a3989164c27d98e72d4fd4641db98b10ea549057
jiyoung-B <catgirl0313@naver.com>
2023-04-05 12:31:23 +0900

    연습문제 풀이

--- src/main/java/examples/EX01.java
+++ src/main/java/examples/EX01.java
@@ -2,72 +2,48 @@ package examples;
 
 public class EX01 {
     public static void main(String[] args) {
-        System.out.println("*   *    **    ****     ****    *   *          /////");
-        System.out.println("*   *   *  *   *   *    *   *   *   *        | o  o |");
-        System.out.println("*****  *    *  ****     ****     * *        (|  ^   |)");
-        System.out.println("*   *  ******  *   *    *   *     *          | [__] |");
-        System.out.println("*   *  *    *  *    *   *    *    *           -----  ");
+        System.out.println("*   *    **    ****    ****    *   *      /////    ");
+        System.out.println("*   *   *  *   *   *   *   *   *   *     | o o |   ");
+        System.out.println("*****  *    *  ****    ****     * *     (|  ^  |)  ");
+        System.out.println("*   *  ******  *   *   *   *     *       | [_] |   ");
+        System.out.println("*   *  *    *  *    *  *    *    *        _____    ");
+
+        System.out.println("  ");
+        System.out.println("            +---+ ");
+        System.out.println("            |   | ");
+        System.out.println("        +---+---+ ");
+        System.out.println("        |   |   | ");
+        System.out.println("    +---+---+---+      /\\_/\\       -----     ");
+        System.out.println("    |   |   |   |     ( ' ' )    / Hello \\   ");
+        System.out.println("+---+---+---+---+     (  -  )   <  Junior |  ");
+        System.out.println("|   |   |   |   |      | | |     \\ Coder /   ");
+        System.out.println("+---+---+---+---+     (__|__)      -----     ");
 
 
-        System.out.println("            +---+                                      ");
-        System.out.println("            |   |                                      ");
-        System.out.println("        +---+---+                                      ");
-        System.out.println("        |   |   |                                      ");
-        System.out.println("    +---+---+---+      /\\_/\\         -------         ");
-        System.out.println("    |   |   |   |     ( ` ` )      / Hello   \\        ");
-        System.out.println("+---+---+---+---+     (  -  )     <  Junior   |        ");
-        System.out.println("|   |   |   |   |      | | |       \\ Coder!  /        ");
-        System.out.println("+---+---+---+---+    (___|___)       -------           ");
+        int soju = 3000;
+        int sj_qty = 2;
+        int chicken = 12000;
+        int chck_qty = 1;
 
+        int total = (soju * sj_qty) + (chicken * chck_qty);
+        double vat = total * 0.1;
 
+        int money = 50000;
+        int charge = money - total;
 
-        //물품
-        String soju = "소주";
-        String chicken = "너나치킨";
-        //양
-        int sojuAmount = 2;
-        int chickenAmount = 1;
-        //가격
-        int sojuPrice = 3000;
-        int chickenPrice = 12000;
-        //품목별 합계 가격
-        int sojusPrice = sojuPrice * sojuAmount;
-        int chickensPrice = chickenPrice * chickenAmount;
-        //합계
-        int total = sojuPrice*sojuAmount + chickenPrice*chickenAmount;
-        //부가세
-        double vat = total * 10 / 100;
-        //받은금액
-        int takedMoney = 50000;
-        //잔돈
-        int change = takedMoney - total;
-        //과세합계
-        double taxTotal = total - vat;
-//        //날짜
-//        let date = new Date();
-//        let year = date.getFullYear();
-//        let month = date.getMonth()+1;
-//        let ndate = date.getDate();
-//        let hours = date.getHours();
-//        let minutes = date.getMinutes();
-//        let seconds = date.getSeconds();
-        
-        
-
-        System.out.println("[ 음식나라 ]");
-        System.out.print("\n -----------------------------");
-        System.out.printf("\n 소주\t\t%2d\t\t%5d", sojuAmount, sojusPrice);
-        System.out.printf("\n 너나치킨\t %d\t\t%5d", chickenAmount, chickensPrice);
-        System.out.print("\n -----------------------------");
-        System.out.printf("\n 과세합계\t\t\t%5.0f", taxTotal);
+        System.out.println("  ");
+        System.out.println(" [ 음식나라 ] ");
+        System.out.print(" ------------------------ ");
+        System.out.printf("\n 소주\t\t%2d\t\t%5d", sj_qty, soju*sj_qty);
+        System.out.printf("\n 너나치킨\t %d\t\t%5d", chck_qty, chicken*chck_qty);
+        System.out.print("\n ------------------------ ");
+        System.out.printf("\n 과세합계\t\t\t%5d", total);
         System.out.printf("\n 부가세\t\t\t\t%5.0f", vat);
-        System.out.print("\n -----------------------------");
-        System.out.printf("\n 총합계\t\t\t\t%5d", total);
-        System.out.printf("\n 받은금액\t\t\t%d", takedMoney);
-        System.out.printf("\n 잔돈\t\t\t\t%d", change);
-        System.out.print("\n -----------------------------");
-        System.out.println(" 2023 .0 4. 03 \t 10:06:24");
-//        System.out.printf(\n year.month.ndate hours:minutes:seconds);
-        
+        System.out.print("\n ------------------------ ");
+        System.out.printf("\n 총합계\t\t\t\t%d", total);
+        System.out.printf("\n 받은금액\t\t\t%d", money);
+        System.out.printf("\n 잔돈\t\t\t\t%d", charge);
+        System.out.print("\n ------------------------ ");
+        System.out.println(" 2023. 04. 03\t 10:06:24 ");
     }
 }
