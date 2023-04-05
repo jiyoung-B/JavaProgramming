243a20751e72bae88319fa88961f6304102059c7
jiyoung-B <catgirl0313@naver.com>
2023-04-05 12:33:17 +0900

    연산자, 형변환, Scanner

--- src/main/java/jy/C03TypeConversion.java
+++ src/main/java/jy/C03TypeConversion.java
@@ -1,39 +1,37 @@
 package jy;
 
 public class C03TypeConversion {
-
     public static void main(String[] args) {
         // 자료형 변환
-        // 묵시적 변환 (auteo, promotion)
-        // 암시적 변환 (casting, overflow
+        // 묵시적 형변환 (auto, promotion)
+        // 명시적 형변환 (casting, overflow)
 
         // 묵시적 형변환
-        byte z = 10;        // byte는 1byte 크기의 자료형
-        // int a = z;       // int는 4byte 크기 자료형 (승격promotion)
-        int a = (int) z;     // 변수 승격시 명시적 형변환 사용시 불필요!
+        byte z = 10;      // byte는 1byte 크기 자료형
+        // int a = z;     // int는 4byte 크기 자료형 (승격promotion)
+        int a = (int) z;  // 변수 승격시 명시적 형변환 사용은 불필요!
 
         System.out.printf("%d, %d\n", z, a);
 
         // 명시적 형변환 : cast연산자 사용!
         // 자료형 변수명A = (변환할자료형) 변수명B;
-
         z = (byte) a;
         System.out.printf("%d, %d\n", z, a);
 
         a = 512;
         z = (byte) a;
-        System.out.printf("%d, %d\n",z, a);
+        System.out.printf("%d, %d\n", z, a);
 
         double b = 3.14;
         long c = (long) b;
-        System.out.printf("%f, %d\n",b, c);
+        System.out.printf("%f, %d\n", b, c);
 
         // 의도적인 형변환
         // 정수끼리 산술연산 결과 : 정수
         // 하지만, 정수끼리 산술연산 결과를 실수로 출력 - 형변환필요!
-
         int x = 98;
         int y = 99;
+
         System.out.printf("98 / 99 = %d\n", x / y);
         // 정수끼리 연산시 결과도 정수 : 소수부 출력 불가!
 
--- src/main/java/jy/C04Scanner.java
+++ src/main/java/jy/C04Scanner.java
@@ -1,53 +1,47 @@
 package jy;
 
-import java.util.Scanner;   // 1
+import java.util.Scanner;    // 1
 
 public class C04Scanner {
-
     public static void main(String[] args) {
         // 데이터 입력 : Scanner
-        Scanner sc = new Scanner(System.in);    // 2
+        Scanner sc = new Scanner(System.in);   // 2
 
         // 이름을 입력받음
-//        System.out.println("이름은?");
-//        String name = sc.next();    // 3
-//        System.out.println("실수는?");
-//        float fl = sc.nextFloat();    // 3
-//        System.out.printf("\n이름은 %s 입니다", name);
-//        System.out.printf("\n이름은 %.3f 입니다", fl);
+        //System.out.println("이름은?");
+        //String name = sc.next();               // 3
+        //System.out.printf("이름은 %s 입니다", name);
 
 
         // ex) 성적 처리프로그램 v2
         // 이름, 국어, 영어, 수학을 키보드로 입력받아
         // 총점, 평균, 학점을 계산한 뒤 결과 출력
         System.out.print("이름은? ");
-        String fname = sc.next();
-        System.out.print("국어점수? ");
+        String name = sc.next();
+        System.out.print("국어는? ");
         int kor = sc.nextInt();
-        System.out.print("영어점수? ");
+        System.out.print("영어는? ");
         int eng = sc.nextInt();
-        System.out.print("수학점수? ");
-        int math = sc.nextInt();
+        System.out.print("수학은? ");
+        int mat = sc.nextInt();
 
-        int tot = kor + eng + math;
-        double avg = (double)tot / 3;
-        char grade = avg >= 90 ? '수' :
-                     avg >= 80 ? '우' :
-                     avg >= 70 ? '미' :
-                     avg >= 60 ? '양' : '가';
+        int tot = kor + eng + mat;
+        double avg = (double) tot / 3;
+        char grd = avg >= 90 ? '수' :
+                avg >= 80 ? '우' :
+                        avg >= 70 ? '미' :
+                                avg >= 60 ? '양' : '가';
 
-
-
-        System.out.printf("\n %s, %d, %d, %d ", fname, kor, eng, math);
-        System.out.printf("\n 총점: %d, 평균 : %.3f, 학점 : %s ", tot, avg, grade);
-
-
-
-
+        System.out.printf("이름 : %s\n", name);
+        System.out.printf("국어 : %d\n", kor);
+        System.out.printf("영어 : %d\n", eng);
+        System.out.printf("수학 : %d\n", mat);
+        System.out.printf("총점 : %d\n", tot);
+        System.out.printf("평균 : %.1f\n", avg);
+        System.out.printf("학점 : %c\n", grd);
 
 
         // 종료시 sc 객체 제거
-        sc.close();     // 4
-
+        sc.close();                           // 4
     }
 }
