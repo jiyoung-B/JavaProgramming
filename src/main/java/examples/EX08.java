26fb763fe355f1d62ec07f6ab3fef9e7ef650561
jiyoung-B <catgirl0313@naver.com>
2023-04-07 15:47:39 +0900

    객체지향 프로그래밍 1

--- src/main/java/examples/EX08.java
+++ src/main/java/examples/EX08.java
@@ -1,61 +1,151 @@
 package examples;
 
-public class EX08 {
+    public class EX08 {
+        public static void main(String[] args) {
+            // 학생Student 클래스를 선언하고 객체 초기화한 후 출력
+            Student std1 = new Student(
+                    201350050, "김태희", "경기도고양시",
+                    "1985.3.22", "컴퓨터", 504);
+
+            //System.out.printf("%d %s %s %s %s %d \n",
+            //    std1.hakbun, std1.name, std1.addr, std1.birth,
+            //    std1.dept, std1.prof);
+
+            System.out.printf("%d %s %s %s %s %d \n",
+                    std1.getHakbun(), std1.getName(), std1.getAddr(),
+                    std1.getBirth(), std1.getDept(), std1.getProf());
+
+            System.out.println(std1);
+
+            // 교수Professor 클래스를 선언하고 객체 초기화한 후 출력
+            Professor p1 =
+                    new Professor(301, "이순신", "프로그래밍");
+
+            //System.out.printf("%d %s %s \n", p1.prof, p1.name, p1.major);
+            System.out.printf("%d %s %s \n",
+                    p1.getProf(), p1.getName(), p1.getMajor());
+
+            System.out.println(p1);
+
+        }
+
+    }
+
     class Student {
+        private int hakbun;
+        private String name;
+        private String addr;
+        private String birth;
+        private String dept;
+        private int prof;
 
-        int 학번;
-        String 이름;
-        String 주소;
-        int 생년월일;
-        String 학과명;
-        int 교수;
+        public Student() {}
 
-        public Student() {
+        public Student(int hakbun, String name, String addr,
+                       String birth, String dept, int prof) {
+            this.hakbun = hakbun;
+            this.name = name;
+            this.addr = addr;
+            this.birth = birth;
+            this.dept = dept;
+            this.prof = prof;
         }
 
-        public Student(int 학번, String 이름, String 주소, int 생년월일, String 학과명, int 교수) {
-            this.학번 = 학번;
-            this.이름 = 이름;
-            this.주소 = 주소;
-            this.생년월일 = 생년월일;
-            this.학과명 = 학과명;
-            this.교수 = 교수;
+        // setter
+        public void setHakbun(int hakbun) {
+            this.hakbun = hakbun;
         }
 
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setAddr(String addr) {
+            this.addr = addr;
+        }
+
+        public void setDept(String dept) {
+            this.dept = dept;
+        }
+
+        public void setProf(int prof) {
+            this.prof = prof;
+        }
+
+        // getter
+        public int getHakbun() {
+            return hakbun;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAddr() {
+            return addr;
+        }
+
+        public String getBirth() {
+            return birth;
+        }
+
+        public String getDept() {
+            return dept;
+        }
+
+        public int getProf() {
+            return prof;
+        }
+
+        // toString
+        @Override
         public String toString() {
-            return "학번: " + 학번 + ", 이름: " + 이름 + ", 주소: " + 주소 + ", 생년월일: " + 생년월일 + ", 학과명: " + 학과명 + ", 교수: " + 교수;
+            String fmt = "%d %s %s %s %s %d\n";
+            return String.format(fmt, hakbun, name, addr, birth, dept, prof);
+        }
+    }
+
+    class Professor {
+        private int prof;
+        private String name;
+        private String major;
+
+        public Professor() {}
+
+        public Professor(int prof, String name, String major) {
+            this.prof = prof;
+            this.name = name;
+            this.major = major;
         }
 
+        // setter
+        public void setProf(int prof) {
+            this.prof = prof;
+        }
 
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setMajor(String major) {
+            this.major = major;
+        }
+
+        public int getProf() {
+            return prof;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getMajor() {
+            return major;
+        }
+
+        @Override
+        public String toString() {
+            String fmt = "%d %s %s";
+            return String.format(fmt, prof, name, major);
+        }
     }
-
-    public static void main(String[] args) {
-        EX08 ex08 = new EX08();
-        Student student = ex08.new Student(201350050, "김태희", "경기도고양시", 19850322, "컴퓨터", 504);
-        //Student student1 = new Student(201350050, "김태희", "경기도고양시", 19850322, "컴퓨터", 504);
-        System.out.println(student);
-
-        Professor professor = new Professor(0205, "이순신", "자바 프로그래밍");
-       // Professor professor = ex08.new Professor(0205, "이순신", "자바 프로그래밍");
-        System.out.println(professor);
-    }
-}
-class Professor{
-    int 교수번호;
-    String 이름;
-    String 전공분야;
-
-    public Professor() {
-    }
-
-    public Professor(int 교수번호, String 이름, String 전공분야) {
-        this.교수번호 = 교수번호;
-        this.이름 = 이름;
-        this.전공분야 = 전공분야;
-    }
-
-    public String toString(){
-        return "교수번호: " + 교수번호 + ", 이름: " + 이름 + ", 전공분야: " + 전공분야;
-
-    }
-}
--- src/main/java/jy/J12OOP.java
+++ src/main/java/jy/J12OOP.java
@@ -2,45 +2,59 @@ package jy;
 
 public class J12OOP {
     public static void main(String[] args) {
-        Person2 p2 = new Person2("지현", "배우", 32, '여');
+        Person2 p1 = new Person2();
 
-        p2.setName("혜교");
-        p2.setJob("가수");
-        p2.setAge(35);
-        p2.setGender('남');
+        // 객체 멤버변수 초기화 - 실패 - private 때문
+        //p1.name = "혜교";
+        //p1.job = "학생";
+        //p1.age = 35;
+        //p1.gender = '여';
 
-        System.out.println(p2.getName());
-        System.out.println(p2.getJob());
-        System.out.println(p2.getAge());
-        System.out.println(p2.getGender());
+        // setter로 변수 초기화
+        p1.setName("혜교");
+        p1.setJob("학생");
+        p1.setAge(35);
+        p1.setGender('여');
 
-        System.out.println(p2);
+        // 객체 출력 1
+        System.out.println(p1.getName());
+        System.out.println(p1.getJob());
+        System.out.println(p1.getAge());
+        System.out.println(p1.getGender());
 
+        // 객체 출력 2
+        System.out.println(p1);
 
     }
 }
-class Person2{
-    private  String name;
-    private  String job;
-    private  int age;
-    private  char gender;
 
-    public Person2(){}// 기본생성자
+class Person2 {
+    // 멤버변수 캡슐화
+    // 접근제한자를 이용해서 멤버변수의 직접 접근을 막음
+    private String name;
+    private String job;
+    private int age;
+    private char gender;
+
+    public Person2() { }
 
     // getter / setter 선언
-    // private 으로 선언된 변수를 외부에 접근할 수 있도록
+    // private으로 선언된 변수를 외부에 접근할 수 있도록
     // 정의하는 특수한 메서드
 
     // getXxx
     public String getName() {
         return name;
     }
+
     public String getJob() {
         return job;
     }
+
     public int getAge() {
         return age;
     }
+
     public char getGender() {
         return gender;
     }
@@ -49,45 +63,23 @@ class Person2{
     public void setName(String name) {
         this.name = name;
     }
+
     public void setJob(String job) {
         this.job = job;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public void setGender(char gender) {
         this.gender = gender;
     }
 
-//    @Override
-//    public String toString() {
-//        return "Person2{" +
-//                "name='" + name + '\'' +
-//                ", job='" + job + '\'' +
-//                ", age=" + age +
-//                ", gender=" + gender +
-//                '}';
-//    }
-//
-
-    // ctrl + spacebar
+    // toString : 모든 멤버변수의 값을 출력할때 사용하는 메서드
     @Override
     public String toString() {
-        String fmt = "";
-        return "Person2{" +
-                "name='" + name + '\'' +
-                ", job='" + job + '\'' +
-                ", age=" + age +
-                ", gender=" + gender +
-                '}';
+        String fmt = "%s %s %d %s\n";
+        return String.format(fmt, name, job, age, gender);
     }
-
-    public Person2(String name, String job, int age, char gender) {
-        this.name = name;
-        this.job = job;
-        this.age = age;
-        this.gender = gender;
-    }// 매개변수 있는 생성자
 }
-
-
