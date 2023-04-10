3afdf4bbd11236c9c13aa9b66d79f8c900acacbc
jiyoung-B <catgirl0313@naver.com>
2023-04-10 13:15:26 +0900

    도서 쇼핑몰 예제 v2 service 클래스 작성 StringBuilder 사용 메뉴출력과 처리를 별개의 클래스로 작성

--- src/main/java/jy/BookMarket.java
+++ src/main/java/jy/BookMarket.java
@@ -1,10 +1,16 @@
 package jy;
 
 public class BookMarket {
-    public static void main(String[] args){
-        System.out.println("Welcome to Shopping Mall");
-        System.out.println("Welcome to Book Market!");
+    public static void main(String[] args) {
+        BookMarketService bksrv = new BookMarketService();
+        while (true) {
+            // bookmarket 메뉴 작성
+            String menu = bksrv.displayMenu();
+
+            // 입력한 메뉴에 따라 작업 분기
+            bksrv.processMenu(menu);
+
+        }
+
     }
-
-
 }
