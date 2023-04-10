ada6949361561ff55a8932961e13b060f2393662
jiyoung-B <catgirl0313@naver.com>
2023-04-10 12:49:02 +0900

    메서드 다중정의, 접근제한자

--- src/main/java/jy/J15OOP.java
+++ src/main/java/jy/J15OOP.java
@@ -16,22 +16,25 @@ public class J15OOP {
     public static void main(String[] args) {
         // 회원처리 기능이 정의된 클래스 객체 선언
         MemberService msrv = new MemberService();
-        Member m = new Member("jy", "010", "1234", "5678",
-                "abc123", "xyz987.com");
+
+        Member m = new Member(
+                "홍길동","010","1234","5678",
+                "abc123","xyz987.com");
 
         msrv.newMember(m);
         msrv.readMember();
         msrv.readOneMember("홍길동");
         msrv.modifyMember("홍길동");
         msrv.removeMember("홍길동");
-
     }
 }
+
+// 회원정보 : 이름, 전화번호, 이메일
 class Member {
     private String name;
-    private String hp1;
-    private String hp2;
-    private String hp3;
+    private String hp1;    // 지역번호
+    private String hp2;    // 국번
+    private String hp3;    // 국번
     private String email1;
     private String email2;
 
@@ -95,29 +98,33 @@ class Member {
 
 // 회원정보와 관련된 CRUD 기능 구현
 class MemberService {
-    // 회원 생성 : new~
-    public boolean newMember(Member mvo){
-        System.out.println("회원정보 생성"+mvo);
+    // 회원 생성C : new~
+    public boolean newMember(Member mvo) {
+        System.out.println("회원정보 생성");
         return false;
     }
+
     // 회원 조회R : read~
     public Member[] readMember() {
         System.out.println("회원정보 리스트 조회");
         return null;
     }
+
     // 회원 조회R : readOne~
-    public Member readOneMember(String name){
-        System.out.println("회원정보 상세 조회"+name);
+    public Member readOneMember(String name) {
+        System.out.println("회원정보 상세 조회");
         return null;
     }
-    // 회원수정U : modify~
-    public boolean modifyMember(String name){
-        System.out.println("회원정보 수정"+name);
+
+    // 회원 수정U: modify~
+    public boolean modifyMember(String name) {
+        System.out.println("회원정보 수정");
         return false;
     }
-    // 회원삭제D : remove~
-    public boolean removeMember(String name){
-        System.out.println("회원정보 삭제"+name);
+
+    // 회원 삭제D: remove~
+    public boolean removeMember(String name) {
+        System.out.println("회원정보 삭제");
         return false;
     }
 }
